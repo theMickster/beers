4a527fa56b6d3ec5bf57688937937ea18f3c4830
Mick Letofsky <mickl@knowledgeservices.com>
2023-11-03 10:59:56 -0600

    Update common, constants for metadata access

--- api/src/Beers.Common/Constants/BeerMetadataPartitionKeyConstants.cs
+++ api/src/Beers.Common/Constants/BeerMetadataPartitionKeyConstants.cs
@@ -2,7 +2,15 @@
 
 public static class BeerMetadataPartitionKeyConstants
 {
-    public static Guid BeerTypeGuid = new("752d4d23-548e-4d1a-9a41-badccbbd7dd9");
+    public static readonly Guid BeerTypeGuid = new("752d4d23-548e-4d1a-9a41-badccbbd7dd9");
 
-    public static string BeerType = BeerTypeGuid.ToString();
+    public static readonly string BeerType = BeerTypeGuid.ToString();
+
+    public static readonly Guid BeerStyleGuid = new("1253cac1-9950-42fd-9b8f-0a44ba7ce5ce");
+
+    public static readonly string BeerStyle = BeerStyleGuid.ToString();
+
+    public static readonly Guid BeerCategoryGuid = new("29d52320-4a29-4181-856e-562501e3f49c");
+
+    public static readonly string BeerCategory = BeerCategoryGuid.ToString();
 }
