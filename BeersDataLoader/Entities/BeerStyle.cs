84e5e122b8e19d623ba522ebefba2309cc6437d8
Mick Letofsky <mickl@knowledgeservices.com>
2024-10-04 12:17:42 -0600

    Create process to import beer data

---
+++ BeersDataLoader/Entities/BeerStyle.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace BeersDataLoader.Entities;
+
+internal class BeerStyle
+{
+    [JsonProperty("id")]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid TypeId { get; set; } = PartitionKeyConstants.BeerStyleGuid;
+
+    public string TypeName = PartitionKeyConstants.BeerStyle;
+
+    public string Name { get; set; } = string.Empty;
+
+    public Guid MetadataId { get; set; } = Guid.NewGuid();
+
+    public string ApplicationName { get; set; } = "Beers";
+}
---
+++ BeersDataLoader/Entities/BreweryType.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace BeersDataLoader.Entities;
+
+internal class BreweryType
+{
+    [JsonProperty("id")]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public Guid TypeId { get; set; } = PartitionKeyConstants.BreweryTypeGuid;
+
+    public string TypeName = PartitionKeyConstants.BreweryType;
+
+    public string Name { get; set; } = string.Empty;
+
+    public Guid MetadataId { get; set; } = Guid.NewGuid();
+
+    public string ApplicationName { get; set; } = "Beers";
+}
